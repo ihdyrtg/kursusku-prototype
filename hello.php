4f4b0d08e72a898253cf89ecd7b42242693666e7
ihdyrtg <ihdi.syahputra@gmail.com>
2026-09-02 15:36:19 +0700

    praktikum-2

--- hello.php
+++ hello.php
@@ -1,13 +1,25 @@
 <!DOCTYPE html>
 <html lang="id">
 <head>
-  <meta charset="UTF-8">
-  <title>Kursusin</title>
+    <meta charset="UTF-8">
+    <title>Kursusin</title>
 </head>
+
 <body>
-  <h1>Selamat datang di Kursusin</h1>
-  <p>Temukan kursus terbaik untukmu.</p>
-  <img src="logo.png" alt="Logo Kursusin" class="img-fluid" style="max-height: 40px;">
-  <a href="praktikum-1.html">Praktikum-1</a>
+
+    <h1>Selamat datang di Kursusin</h1>
+
+    <p>Temukan kursus terbaik untukmu.</p>
+
+    <img 
+        src="../assets/img/logo.png" 
+        alt="Logo Kursusin" 
+        style="max-height: 60px;"
+    >
+
+    <br><br>
+
+    <a href="praktikum-1.html">Praktikum-1</a>
+
 </body>
 </html>
